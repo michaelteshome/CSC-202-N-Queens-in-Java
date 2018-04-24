1f8c14cd07ac700583772bf4c33e22cfe0109e8f
Michael Teshome <35178796+michaelteshome@users.noreply.github.com>
2018-04-24 10:49:29 -0400

    Add files via upload

---
+++ Maze.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Maze {
+
+	public static void main(String[] args) {
+		
+		Quee q1 = new Quee();
+		q1.read();
+		
+		System.out.println("Plain Maze Board: ");
+		q1.printMaze();
+		
+		System.out.println("Maze Board Runner: ");
+		q1.createMaze();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+}
